d57edbc16320ba6ab18b4efbb2c57879a6da7173
C R SAIRAM <115174268+crs7617@users.noreply.github.com>
2023-10-30 22:58:37 +0530

    Update audioprocessing.m

--- audioprocessing.m
+++ audioprocessing.m
@@ -1,4 +1,4 @@
-% Load an audio signal and 
+% Load an audio signal
 [y, Fs] = audioread('original_audio.wav');  % Replace with your audio file
 [h, Fs_h] = audioread('room_impulse_response.wav');  % Replace with your RIR file
 
